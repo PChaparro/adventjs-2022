3ca792c8b0441da3565305852014546a0b2154cd
Pedro Andres Chaparro Quintero <pedroandreschaparro@gmail.com>
2022-12-01 13:44:10 -0500

    :pencil2: Fix typo

--- challenge-01/index.js
+++ challenge-01/index.js
@@ -1,4 +1,4 @@
-const wraping = (gifts) => {
+const wrapping = (gifts) => {
   return gifts.map((gift) => {
     const padding = '*'.repeat(gift.length + 2);
     return `${padding}\n*${gift}*\n${padding}`;
