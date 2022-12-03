f1ec799c0f7b48e715ea864c8cfda936ee4e0c43
Pedro Andres Chaparro Quintero <pedroandreschaparro@gmail.com>
2022-12-03 12:35:32 -0500

    :sparkles: Alternative solution to `Day 3`

--- challenge-03/index.js
+++ challenge-03/index.js
@@ -4,4 +4,25 @@ const solve = (packOfGifts, reindeers) => {
   return Math.floor(reindeersCapacity / giftsWeight);
 };
 
+/* This solution reach the same score
+const solve = (packOfGifts, reindeers) => {
+  const max = Math.max(packOfGifts.length, reindeers.length);
+  let reindeersCapacity = 0;
+  let giftsWeight = 0;
+
+  for(let i=0; i<max; i++){
+    if(packOfGifts.length > i){
+      giftsWeight += packOfGifts[i].length;
+    }
+
+    if(reindeers.length > i){
+      reindeersCapacity += reindeers[i].length * 2;
+    }
+  }
+
+  return Math.floor(reindeersCapacity / giftsWeight); 
+
+};
+ */
+
 module.exports = { solve };
