0435077126195ab2a238f91c15736b845cbf4eff
Pedro Andres Chaparro Quintero <pedroandreschaparro@gmail.com>
2022-12-07 14:35:31 -0500

    :zap: Less operations creating the set before the filter

--- challenge-07/index.js
+++ challenge-07/index.js
@@ -1,12 +1,8 @@
 const solve = (a1, a2, a3) => {
-  return Array.from(
-    new Set(
-      [...a1, ...a2, ...a3].filter((gift) => {
-        // True = 1, so, we filter the gifts that are only contained in of the lists
-        return a1.includes(gift) + a2.includes(gift) + a3.includes(gift) === 1;
-      }),
-    ),
-  );
+  return Array.from(new Set([...a1, ...a2, ...a3])).filter((gift) => {
+    // True = 1, so, we filter the gifts that are only contained in of the lists
+    return a1.includes(gift) + a2.includes(gift) + a3.includes(gift) === 1;
+  });
 };
 
 module.exports = { solve };
