b8566d9fc1a6a6551978d01a68c12e277123bfc9
Pedro Andres Chaparro Quintero <pedroandreschaparro@gmail.com>
2022-12-18 14:24:04 -0500

    :zap: Remove unnecessary index

--- challenge-18/index.js
+++ challenge-18/index.js
@@ -2,7 +2,7 @@ const solve = (dry, numbers) => {
   return new Array(numbers)
     .fill()
     .map((_, index) => index + 1)
-    .filter((_, index) => `${index + 1}`.includes(`${dry}`));
+    .filter((val) => `${val}`.includes(`${dry}`));
 };
 
 module.exports = { solve };
